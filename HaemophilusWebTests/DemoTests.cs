1905f1e664a218ff09f7d6afef731adf310bc071
Markus Reinhardt <tuxxilla@gmail.com>
2013-10-19 22:16:49 +0200

    Added NUnit test project

---
+++ HaemophilusWebTests/DemoTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace HaemophilusWebTests
+{
+    [TestFixture]
+    public class DemoTests
+    {
+        [Test]
+        public void MehodUnderTest_Scenario_ExpectedBehavior()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
