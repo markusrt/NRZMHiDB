97a854564fc049bb7b5f08774c7a7cc778ef7f04
Markus Reinhardt <tuxxilla@gmail.com>
2013-10-19 23:22:19 +0200

    Removed MsTest project
    * Moved tests to NUnit

---
+++ HaemophilusWebTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace HaemophilusWeb.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index()
+        {
+            var controller = new HomeController();
+            
+            var result = controller.Index() as ViewResult;
+
+            result.Should().NotBeNull();
+        }
+
+        [Test]
+        public void About()
+        {
+            var controller = new HomeController();
+
+            var result = controller.About() as ViewResult;
+
+            result.Should().NotBeNull();
+        }
+
+        [Test]
+        public void Contact()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Contact() as ViewResult;
+
+            result.Should().NotBeNull();
+        }
+    }
+}
