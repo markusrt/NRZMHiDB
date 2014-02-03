d2ee5fc38b05ee3f3585b45f72bc8e059be057aa
Markus Reinhardt <tuxxilla@gmail.com>
2014-02-03 23:34:06 +0100

    Refactoring: Defined editor template for simple text fields
    * Support for placeholder
    * Support for required attribute
    
    Used editor template in patient create or edit

---
+++ HaemophilusWeb/Views/Utils/BootstrapEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace HaemophilusWeb.Views.Utils
+{
+    public static class BootstrapEditor
+    {
+        private const string FormGroupTemplate = "<div class=\"form-group\">{0}{1}</div>";
+
+        private const string TextBoxTemplate = "<div class=\"col-sm-5\">{0}{1}</div>";
+        
+        private const string RequiredTemplate = "<div class=\"input-group\">{0}<span class=\"input-group-addon\"><span class=\"glyphicon glyphicon-star\"></span></span></div>";
+
+        public static MvcHtmlString TextEditorFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression, string placeholder = null)
+        {
+            var modelMetadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            var required = modelMetadata.IsRequired;
+            var label = htmlHelper.LabelFor(expression, new {@class = "col-sm-2 control-label"}).ToHtmlString();
+            var textBox = htmlHelper.TextBoxFor(expression, new {placeholder, @class = "form-control"}).ToHtmlString();
+            var validationHtml = GetValidationHtml(htmlHelper, expression);
+
+            if (required)
+            {
+                textBox = string.Format(RequiredTemplate, textBox);
+            }
+
+            var textBoxHtml = string.Format(TextBoxTemplate, textBox, validationHtml);
+
+            return MvcHtmlString.Create(string.Format(FormGroupTemplate, label, textBoxHtml));
+        }
+
+        private static string GetValidationHtml<TModel, TProperty>(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            var validationMessage = htmlHelper.ValidationMessageFor(expression);
+            if (validationMessage == null)
+            {
+                return string.Empty;
+            }
+            return validationMessage.ToHtmlString();
+        }
+    }
+}
---
+++ HaemophilusWebTests/Views/Utils/BootstrapEditorTests.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace HaemophilusWeb.Views.Utils
+{
+    public class BootstrapEditorTests
+    {
+        private class SimpleModel
+        {
+            public string SimpleProperty { get; set; }
+
+            [Required]
+            public string RequiredProperty { get; set; }
+        }
+
+        [Test]
+        public void TextEditorFor_SimpleProperty_CreatesBootstrapTextEditor()
+        {
+            var helper = CreateHtmlHelper<SimpleModel>(new ViewDataDictionary(new SimpleModel()));
+            var textEditorHtml = helper.TextEditorFor(m => m.SimpleProperty, "Placeholder");
+
+            textEditorHtml.ToHtmlString().Should().Match("*<label*for*SimpleProperty*");
+            textEditorHtml.ToHtmlString().Should().Match("*<input*form-control*id=\"SimpleProperty\" name=\"SimpleProperty\"*");
+            textEditorHtml.ToHtmlString().Should().Match("*placeholder=\"Placeholder\"*");
+            
+        }
+
+        [Test]
+        public void TextEditorFor_SimpleProperty_ShouldNotContainGlyphicons()
+        {
+            var helper = CreateHtmlHelper<SimpleModel>(new ViewDataDictionary(new SimpleModel()));
+            var textEditorHtml = helper.TextEditorFor(m => m.SimpleProperty);
+
+            textEditorHtml.ToHtmlString().Should().NotContain("input-group-addon");
+            textEditorHtml.ToHtmlString().Should().NotContain("glyphicon glyphicon-star");
+        }
+
+        [Test]
+        public void TextEditorFor_RequiredProperty_ShouldContainGlyphiconStar()
+        {
+            var helper = CreateHtmlHelper<SimpleModel>(new ViewDataDictionary(new SimpleModel()));
+            var textEditorHtml = helper.TextEditorFor(m => m.RequiredProperty);
+
+            textEditorHtml.ToHtmlString().Should().Contain("input-group-addon");
+            textEditorHtml.ToHtmlString().Should().Contain("glyphicon glyphicon-star");
+        }
+
+        public HtmlHelper<T> CreateHtmlHelper<T>(ViewDataDictionary viewData)
+        {
+            var cc = new Mock<ControllerContext>(
+                new Mock<HttpContextBase>().Object,
+                new RouteData(),
+                new Mock<ControllerBase>().Object);
+
+            var mockViewContext = new Mock<ViewContext>(
+                cc.Object,
+                new Mock<IView>().Object,
+                viewData,
+                new TempDataDictionary(),
+                TextWriter.Null);
+
+            mockViewContext.Setup(c => c.ViewData).Returns(viewData);
+
+            mockViewContext.Object.ViewData = viewData; 
+
+            var mockViewDataContainer = new Mock<IViewDataContainer>();
+
+            mockViewDataContainer.Setup(v => v.ViewData).Returns(viewData);
+
+            return new HtmlHelper<T>(
+                mockViewContext.Object, mockViewDataContainer.Object);
+        }
+    }
+}
