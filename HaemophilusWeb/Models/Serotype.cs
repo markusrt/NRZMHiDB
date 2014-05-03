90f547a9e8cfb40c413401fbb8d78999a509e11a
Markus Reinhardt <tuxxilla@gmail.com>
2014-05-03 17:49:40 +0200

    Fixed typo in serotype description

--- HaemophilusWeb/Models/Serotype.cs
+++ HaemophilusWeb/Models/Serotype.cs
@@ -5,11 +5,17 @@ namespace HaemophilusWeb.Models
     public enum Serotype
     {
         [Description("n.d.")] NotDetermined = 0,
+        [Description("a")]
         A = 1,
+        [Description("b")]
         B = 2,
+        [Description("c")]
         C = 3,
+        [Description("d")]
         D = 4,
+        [Description("e")]
         E = 5,
+        [Description("f")]
         F = 6,
         [Description("Negativ")] Negative = 7,
     }
