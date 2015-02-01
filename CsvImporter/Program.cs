1af4e214b2da27054ccaa6d83db157ae43966b68
Markus Reinhardt <tuxxilla@gmail.com>
2015-02-01 20:09:39 +0100

    Import finished

--- CsvImporter/Program.cs
+++ CsvImporter/Program.cs
@@ -21,31 +21,31 @@ namespace CsvImporter
                 Db.Sendings.Include(s => s.Patient)
                     .Include(s => s.Isolate)
                     .Include(s => s.Isolate.EpsilometerTests.Select(e => e.EucastClinicalBreakpoint));
-            foreach (var sending in sendings)
-            {
-                var csvRecord = new SendingCsvRecord();
-                csvRecord.PopulateWith(sending);
-                csvRecord.PopulateWith(sending.Patient);
-                csvRecord.PopulateWith(sending.Isolate);
+            //foreach (var sending in sendings)
+            //{
+            //    var csvRecord = new SendingCsvRecord();
+            //    csvRecord.PopulateWith(sending);
+            //    csvRecord.PopulateWith(sending.Patient);
+            //    csvRecord.PopulateWith(sending.Isolate);
 
-                PopulateEpsilometerTestResult(sending, Antibiotic.Ampicillin,
-                    _ => csvRecord.AmpicillinEpsilometerTestResult = _,
-                    _ => csvRecord.AmpicillinMeasurement = _);
+            //    PopulateEpsilometerTestResult(sending, Antibiotic.Ampicillin,
+            //        _ => csvRecord.AmpicillinEpsilometerTestResult = _,
+            //        _ => csvRecord.AmpicillinMeasurement = _);
 
-                PopulateEpsilometerTestResult(sending, Antibiotic.AmoxicillinClavulanate,
-                    _ => csvRecord.AmoxicillinClavulanateEpsilometerTestResult = _,
-                    _ => csvRecord.AmoxicillinClavulanateMeasurement = _);
+            //    PopulateEpsilometerTestResult(sending, Antibiotic.AmoxicillinClavulanate,
+            //        _ => csvRecord.AmoxicillinClavulanateEpsilometerTestResult = _,
+            //        _ => csvRecord.AmoxicillinClavulanateMeasurement = _);
 
-                PopulateEpsilometerTestResult(sending, Antibiotic.Cefotaxime,
-                    _ => csvRecord.CefotaximeEpsilometerTestResult = _,
-                    _ => csvRecord.CefotaximeMeasurement = _);
+            //    PopulateEpsilometerTestResult(sending, Antibiotic.Cefotaxime,
+            //        _ => csvRecord.CefotaximeEpsilometerTestResult = _,
+            //        _ => csvRecord.CefotaximeMeasurement = _);
 
-                PopulateEpsilometerTestResult(sending, Antibiotic.Meropenem,
-                    _ => csvRecord.MeropenemEpsilometerTestResult = _,
-                    _ => csvRecord.MeropenemMeasurement = _);
-                csvRecord.EuCastVersion = "3.1";
-                csvRecords.Add(csvRecord);
-            }
+            //    PopulateEpsilometerTestResult(sending, Antibiotic.Meropenem,
+            //        _ => csvRecord.MeropenemEpsilometerTestResult = _,
+            //        _ => csvRecord.MeropenemMeasurement = _);
+            //    csvRecord.EuCastVersion = "3.1";
+            //    csvRecords.Add(csvRecord);
+            //}
             var sendingsCsv = CsvSerializer.SerializeToCsv(csvRecords);
 
             File.WriteAllText(@"c:\temp\import.csv", sendingsCsv, Encoding.GetEncoding("UTF-8"));
