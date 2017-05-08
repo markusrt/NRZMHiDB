1f3feed181711d6f13681781b80e84bb903a6132
Markus Reinhardt <tuxxilla@gmail.com>
2017-05-08 21:54:09 +0200

    Unified editor for DateTime

--- HaemophilusWeb/Views/Utils/BootstrapEditor.cs
+++ HaemophilusWeb/Views/Utils/BootstrapEditor.cs
@@ -9,6 +9,8 @@ namespace HaemophilusWeb.Views.Utils
     {
         private const string FormGroupTemplate = "<div class=\"form-group\">{0}{1}</div>";
 
+        private const string FormGroupTemplateWithId = "<div id=\"{2}\" class=\"form-group\">{0}{1}</div>";
+
         private const string ColSm5 = "col-sm-5";
 
         private const string DivSmX = "<div class=\"{0}\">{1}{2}</div>";
@@ -18,6 +20,8 @@ namespace HaemophilusWeb.Views.Utils
         private const string DivSmFive = "<div class=\"col-sm-5\">{0}{1}</div>";
         
         private const string RequiredTemplate = "<div class=\"input-group\">{0}<span class=\"input-group-addon\"><span class=\"glyphicon glyphicon-star\"></span></span></div>";
+
+        private const string DateTemplate = "<div class=\"input-group\">{0}<span class=\"input-group-addon\"><span class=\"glyphicon glyphicon-calendar\"></span></span></div>";
 
         public static MvcHtmlString TextEditorFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
             Expression<Func<TModel, TProperty>> expression, string placeholder = null)
@@ -36,7 +40,23 @@ namespace HaemophilusWeb.Views.Utils
 
             var textBoxHtml = string.Format(DivSmFive, textBox, validationHtml);
 
-            return MvcHtmlString.Create(string.Format(FormGroupTemplate, label, textBoxHtml));
+            return CreateFormGroup(label, textBoxHtml);
+        }
+
+        public static MvcHtmlString DateEditorFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression, string additionalClass = "", string id = null)
+        {
+            var label = htmlHelper.LabelFor(expression);
+            var modelMetadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            var model = modelMetadata.Model;
+            htmlHelper.EnableClientValidation(false);
+            var textBox = htmlHelper.TextBoxFor(expression, new { @class = "form-control datepicker " + additionalClass, type = "datetime", Value = model == null ? "" : $"{model:dd.MM.yyyy}"}).ToHtmlString();
+            htmlHelper.EnableClientValidation(true);
+            var validationHtml = GetValidationHtml(htmlHelper, expression);
+
+            var textBoxHtml = string.Format(DivSmFive, string.Format(DateTemplate, textBox), validationHtml);
+
+            return CreateFormGroup(label, textBoxHtml, id);
         }
 
 
@@ -48,7 +68,7 @@ namespace HaemophilusWeb.Views.Utils
 
             var enumRadioEditor = string.Format(DivSmX, radioColSmXClass, enumRadioButton, string.Empty);
 
-            return MvcHtmlString.Create(string.Format(FormGroupTemplate, label, enumRadioEditor));
+            return CreateFormGroup(label, enumRadioEditor);
         }
 
         public static MvcHtmlString ReadonlyFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
@@ -59,8 +79,16 @@ namespace HaemophilusWeb.Views.Utils
             var hidden = htmlHelper.HiddenFor(expression).ToHtmlString();
             var readonlyField = string.Format(DivSmXStatic, smXClass, display, hidden);
 
-            return MvcHtmlString.Create(string.Format(FormGroupTemplate, label, readonlyField));
+            return CreateFormGroup(label, readonlyField);
         }
+
+        private static MvcHtmlString CreateFormGroup(string labelHtml, string elementHtml, string id = null)
+        {
+            return id == null
+                ? MvcHtmlString.Create(string.Format(FormGroupTemplate, labelHtml, elementHtml))
+                : MvcHtmlString.Create(string.Format(FormGroupTemplateWithId, labelHtml, elementHtml, id));
+        }
+
         private static string LabelFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
         {
             return htmlHelper.LabelFor(expression, new { @class = "col-sm-2 control-label" }).ToHtmlString();
