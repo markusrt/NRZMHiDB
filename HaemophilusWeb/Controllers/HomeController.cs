a7a0c30710e8a355b932ce389c42af8ec3bc266c
Markus Reinhardt <tuxxilla@gmail.com>
2018-11-09 11:40:35 +0100

    Provide select box for valid Epsilometer test values

--- HaemophilusWeb/Controllers/HomeController.cs
+++ HaemophilusWeb/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace HaemophilusWeb.Controllers
             },
             new List<Change>
             {
+                new Change(new DateTime(2018, 12, 15), "Feste Skala für E-Test Werte", ChangeType.Feature),
                 new Change(new DateTime(2018, 7, 25), "Anpassung des Sicherheitsprotokolles zur Kommunikation mit dem RKI Tool", ChangeType.Bug),
                 new Change(new DateTime(2018, 7, 22), "Update der Interpretationssätze für End- und Teilbefunde.", ChangeType.Feature),
                 new Change(new DateTime(2018, 6, 30), "<p>Einführung von Teilbefunden</p>" +
