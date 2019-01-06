154395f7fe91cee7abee575c9439915fb05358dc
Markus Reinhardt <tuxxilla@gmail.com>
2019-01-06 23:59:41 +0100

    Update postal code query
    As YQL is now finally discontinued the postal code query is now executed
    at controller level. This is required as geonames.org does not offer HTTPS
    out of the box.

--- HaemophilusWeb/Controllers/HomeController.cs
+++ HaemophilusWeb/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace HaemophilusWeb.Controllers
             },
             new List<Change>
             {
+                new Change(new DateTime(2019, 01, 07), "Update der Postleitzahl Abfrage", ChangeType.Feature),
                 new Change(new DateTime(2018, 12, 16), "Korrektur der Interpretationssätze für Endbefunde.", ChangeType.Bug),
                 new Change(new DateTime(2018, 12, 15), "Feste Skala für E-Test Werte", ChangeType.Feature),
                 new Change(new DateTime(2018, 7, 25), "Anpassung des Sicherheitsprotokolles zur Kommunikation mit dem RKI Tool", ChangeType.Bug),
