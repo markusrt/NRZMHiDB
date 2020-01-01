6fee7b842c68e8e266934f40c01d8d8c9a34efec
Markus Reinhardt <tuxxilla@gmail.com>
2020-01-02 00:18:28 +0100

    Update version number

--- HaemophilusWeb/Properties/GlobalAssemblyInfo.cs
+++ HaemophilusWeb/Properties/GlobalAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Markus Reinhardt - IT Services")]
 [assembly: AssemblyProduct("Haemophilus Web")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.15.0.0")]
-[assembly: AssemblyFileVersion("2.15.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
