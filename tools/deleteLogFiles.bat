e7916c6866945fd2473060804ef17844596d99dc
Markus Reinhardt <tuxxilla@gmail.com>
2020-05-14 07:02:06 +0200

    Prevent yes/no prompt in script run

--- tools/deleteLogFiles.bat
+++ tools/deleteLogFiles.bat
@@ -1,5 +1,5 @@
 REM This script needs to run as scheduled task in order to fulfill
 REM data privacy regulations on the application server
 
-ForFiles /p "%SystemDrive%\inetpub\wwwroot\Haemophilus\Logs" /s /d -30 /c "cmd /c del @file"
-ForFiles /p "%SystemDrive%\inetpub\logs\LogFiles" /s /d -30 /c "cmd /c del @file"
+ForFiles /p "%SystemDrive%\inetpub\wwwroot\Haemophilus\Logs" /s /d -30 /c "cmd /c del /q @file"
+ForFiles /p "%SystemDrive%\inetpub\logs\LogFiles" /s /d -30 /c "cmd /c del /q @file"
