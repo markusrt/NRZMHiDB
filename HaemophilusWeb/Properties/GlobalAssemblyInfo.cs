139c2a7a7963e334b6b1db0b72007711b9d9bbbe
Markus Reinhardt <tuxxilla@gmail.com>
2025-10-09 08:05:05 +0200

    feat: Adapt to PubMLST API changes
    See also <https://pubmlst.org/updates>. HTTP requests have been disabled on the API. Also there was an issue with OAuth URL falsely adding POST parameters.

--- HaemophilusWeb/Properties/GlobalAssemblyInfo.cs
+++ HaemophilusWeb/Properties/GlobalAssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.47")]
-[assembly: AssemblyFileVersion("3.47")]
+[assembly: AssemblyVersion("3.48")]
+[assembly: AssemblyFileVersion("3.48")]
